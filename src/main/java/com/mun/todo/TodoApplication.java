3e27b9fcb4bdfb8726c52c78dd21b15424d76c36
mun <mun.xml@gmail.com>
2023-09-19 16:56:12 +0900

    Remove unnecessary code

--- src/main/java/com/mun/todo/TodoApplication.java
+++ src/main/java/com/mun/todo/TodoApplication.java
@@ -13,16 +13,4 @@ public class TodoApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
     }
-
-//    @Bean
-//    public CommandLineRunner runner(TodoRepository todoRepository) throws Exception {
-//        return (args) -> {
-//            IntStream.rangeClosed(1, 10).forEach(index -> todoRepository.save(Todo.builder()
-//                    .content("오늘 할 일 " + index)
-//                    .createdDateTime(LocalDateTime.now())
-//                    .isComplete(false)
-//                    .build()
-//            ));
-//        };
-//    }
 }
